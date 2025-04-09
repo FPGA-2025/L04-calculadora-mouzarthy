2679057f134fe2ab9b0641c84274fc4c41775ce8
Mouzarthy <mouzarthy2007@hotmail.com>
2025-04-09 00:55:35 -0300

    tarefa-calculadora

--- calculadora.v
+++ calculadora.v
@@ -5,6 +5,14 @@ module calculadora(
   output  [7:0] saida
 );
 
-// insira seu código aqui
+wire [7:0] zerar, mostrar_a, mostrar_b, somar, subtrair;
+
+assign zerar      = ( codigo == 3'b000 ) ? 0 : 0;
+assign mostrar_a  = ( codigo == 3'b001 ) ? entrada_A : 0;
+assign mostrar_b  = ( codigo == 3'b010 ) ? entrada_B : 0;
+assign somar      = ( codigo == 3'b011 ) ? entrada_A + entrada_B : 0;
+assign subtrair   = ( codigo == 3'b100 ) ? entrada_A - entrada_B : 0;
+
+assign saida = ( zerar | mostrar_a | mostrar_b | somar | subtrair );
 
 endmodule
